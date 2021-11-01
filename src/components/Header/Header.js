4247a6b8985c5c146999cbf6a8be5001fec1d4c4
Momcilo-Mitrovic <mitrovicmomcio09@gmail.com>
2021-11-01 19:45:42 +0100

    added social-links
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcio09@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import "./Header.css";
-import useDarkMode from "use-dark-mode";
-import Toggle from "../../components/Toggle/Toggle";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './Header.css'
+import useDarkMode from 'use-dark-mode'
+import Toggle from '../../components/Toggle/Toggle'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import IconGithubSVG from './IconGithub'
+import IconLinkedInSVG from './IconLinkedIn'
 
 function Header() {
-  const darkMode = useDarkMode(false);
+  const darkMode = useDarkMode(false)
 
   return (
     <div className="Header">
@@ -29,11 +31,23 @@ function Header() {
               </li>
             </ul>
           </nav>
-          <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
+          <div className="social-links">
+            <a target="_blank" href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/">
+              <span>
+                <IconLinkedInSVG />
+              </span>
+            </a>
+            <a target="_blank" href="https://github.com/momcilo-mitrovic">
+              <span>
+                <IconGithubSVG />
+              </span>
+            </a>
+          </div>
         </header>
       </div>
+      <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
