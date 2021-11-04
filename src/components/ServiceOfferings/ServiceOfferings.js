20bc7f93b93b00ff0a4a62c02b0a3da0f8bfc1d6
Momcilo-Mitrovic <mitrovicmomcio09@gmail.com>
2021-11-04 20:59:12 +0100

    added Service offering
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcio09@gmail.com>

---
+++ src/components/ServiceOfferings/ServiceOfferings.js
@@ -0,0 +1,13 @@
+import './ServiceOfferings.css'
+import Card from '../Card/Card'
+
+function ServiceOfferings() {
+  return (
+    <div className="ServiceOfferings">
+      <h3> What I can Offer</h3>
+      <Card />
+    </div>
+  )
+}
+
+export default ServiceOfferings
