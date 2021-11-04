ac22a661c4c8bbba852c2142f93b6582aaaac904
Schalk Burger <schalkb@gmail.com>
2021-11-04 22:04:17 +0200

    Refactor Card component

---
+++ src/components/Card/Card-Old.js
@@ -0,0 +1,41 @@
+import "./Card.css";
+import IconApps from "./IconApps";
+import IconDesign from "./IconDesign";
+import IconDev from "./IconDev";
+
+function Card() {
+  return (
+    <section className="languages">
+      <div className="container flex">
+        <div className="card">
+          <IconDesign />
+          <h1> Web Design </h1>
+          <h4>
+            Colors, fonts, typography, components... all in one place,
+            documented and easy to access.
+          </h4>
+        </div>
+        <div className="card">
+          <IconDev />
+          <h1> Full Stack Development </h1>
+          <h4>
+            {" "}
+            Cutting-edge and mobile responsive HTML5, CSS and React
+            applications. I've got you covered.{" "}
+          </h4>
+        </div>
+        <div className="card">
+          <IconApps />
+          <h1> App Development </h1>
+          <h4>
+            {" "}
+            With more than 10 years of WordPress experience I can show you what
+            works and what does not.{" "}
+          </h4>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Card;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,13 @@
 import "./Home.css";
-import Card from "../../components/Card/Card";
-import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
-import Title from "../../components/Title/Title";
+import ServiceOfferings from "../../components/ServiceOfferings/ServiceOfferings";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="Home">
       <Hero />
-      <Title />
-      <Card />
-
-      {/*
-
       <ServiceOfferings />
-
-    */}
     </div>
   );
 }
