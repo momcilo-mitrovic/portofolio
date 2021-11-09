60eaf9c3d36f8a296e169ed552876c5f118f0662
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-11-10 00:28:30 +0100

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,29 +1,22 @@
-import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
-import emailjs from "emailjs-com";
+import React, { useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import emailjs from 'emailjs-com'
 
 function Form() {
-  const form = useRef();
+  const form = useRef()
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    emailjs
-      .sendForm(
-        "service_ilxby1c",
-        "template_zzu0tv8",
-        form.current,
-        "user_S0TMnhXi9IKSH4otWECuh"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+    emailjs.sendForm('service_ilxby1c', 'template_zzu0tv8', form.current, 'user_S0TMnhXi9IKSH4otWECuh').then(
+      (result) => {
+        console.log(result.text)
+      },
+      (error) => {
+        console.log(error.text)
+      },
+    )
+  }
 
   return (
     <form ref={form} onSubmit={sendEmail}>
@@ -35,6 +28,6 @@ function Form() {
       <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
-  );
+  )
 }
-export default Form; /* I installed react-hook-form */
+export default Form /* I installed react-hook-form */
