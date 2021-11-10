d34bd70a5bdf047fc47649e6edeb1128883ccc86
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-11-10 01:05:04 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import "./Header.css";
-import useDarkMode from "use-dark-mode";
-import Toggle from "../../components/Toggle/Toggle";
-import { BrowserRouter as Link } from "react-router-dom";
-import IconGithubSVG from "./IconGithub.js";
-import IconLinkedInSVG from "./IconLinkedIn";
+import './Header.css'
+import useDarkMode from 'use-dark-mode'
+import Toggle from '../../components/Toggle/Toggle'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import IconGithubSVG from './IconGithub.js'
+import IconLinkedInSVG from './IconLinkedIn'
 
 function Header() {
-  const darkMode = useDarkMode(false);
+  const darkMode = useDarkMode(false)
 
   return (
     <div className="Header">
@@ -32,20 +32,12 @@ function Header() {
             </ul>
           </nav>
           <div className="social-links">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/"
-            >
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/">
               <span>
                 <IconLinkedInSVG />
               </span>
             </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/momcilo-mitrovic"
-            >
+            <a target="_blank" rel="noreferrer" href="https://github.com/momcilo-mitrovic">
               <span>
                 <IconGithubSVG />
               </span>
@@ -55,7 +47,7 @@ function Header() {
       </div>
       <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
