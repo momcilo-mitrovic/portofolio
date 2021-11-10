f7c5dd1eac4eea196b7c092d714aca50fa2e96be
Schalk Burger <schalkb@gmail.com>
2021-11-10 02:04:03 +0200

    Initial Contact page updates

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,19 @@
-import './Contact.css'
-import Form from '../../components/Form/Form'
+import "./Contact.css";
+import Form from "../../components/Form/Form";
 
 function Contact() {
   return (
     <div className="Contact">
-      <Form />
+      <div className="Contact-header-container">
+        <div className="container Contact-header">
+          <h1>Contact me</h1>
+        </div>
+      </div>
+      <div className="container flex">
+        <Form />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
