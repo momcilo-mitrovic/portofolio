b96c7a0d72b235750801734e8c40a0105af077a2
Schalk Burger <schalkb@gmail.com>
2021-11-21 14:02:19 +0200

    Fix Button and Hero

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Link } from "react-router-dom";
 function Button(props) {
   return (
     <div className="Button">
-      <Link to={props.link}>{props.children}</Link>
+      <a href={props.link}>{props.children}</a>
     </div>
   );
 }
