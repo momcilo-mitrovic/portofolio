5a430b1a39fc6f246032aa35ad4e5e24ad26b120
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-11-24 18:03:26 +0100

    Failed some shit
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import "./Header.scss";
-import useDarkMode from "use-dark-mode";
-import Toggle from "../../components/Toggle/Toggle";
-import { BrowserRouter as Link } from "react-router-dom";
-import IconGithubSVG from "./IconGithub.js";
-import IconLinkedInSVG from "./IconLinkedIn";
+import './Header.scss'
+import useDarkMode from 'use-dark-mode'
+import Toggle from '../../components/Toggle/Toggle'
+import { BrowserRouter as Link } from 'react-router-dom'
+import IconGithubSVG from './IconGithub.js'
+import IconLinkedInSVG from './IconLinkedIn'
 
 function Header() {
-  const darkMode = useDarkMode(false);
+  const darkMode = useDarkMode(false)
 
   return (
     <div className="Header">
@@ -31,21 +31,33 @@ function Header() {
               </li>
             </ul>
           </nav>
+          <nav className="menu-mobile">
+            <input className="menu-btn" type="checkbox" id="menu-btn" />
+            <label className="menu-icon" for="menu-btn">
+              <span className="navicon"></span>
+            </label>
+            <ul className="menu">
+              <li>
+                <a href="#work">Our Work</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#careers">Careers</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
           <div className="social-links">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/"
-            >
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/">
               <span>
                 <IconLinkedInSVG />
               </span>
             </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/momcilo-mitrovic"
-            >
+            <a target="_blank" rel="noreferrer" href="https://github.com/momcilo-mitrovic">
               <span>
                 <IconGithubSVG />
               </span>
@@ -55,7 +67,7 @@ function Header() {
       </div>
       <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
