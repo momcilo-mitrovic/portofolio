cabe9272e323d36281585b53b63aec428907e9be
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-11-24 18:35:38 +0100

    Added hamburger menu
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,37 +31,39 @@ function Header() {
               </li>
             </ul>
           </nav>
-          <nav className="menu-mobile">
-            <input className="menu-btn" type="checkbox" id="menu-btn" />
-            <label className="menu-icon" for="menu-btn">
-              <span className="navicon"></span>
-            </label>
-            <ul className="menu">
-              <li>
-                <a href="#work">Our Work</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#careers">Careers</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="social-links">
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/">
-              <span>
-                <IconLinkedInSVG />
-              </span>
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://github.com/momcilo-mitrovic">
-              <span>
-                <IconGithubSVG />
-              </span>
-            </a>
+          <div className="menu-mobile-container">
+            <nav className="menu-mobile">
+              <input className="menu-btn" type="checkbox" id="menu-btn" />
+              <label className="menu-icon" for="menu-btn">
+                <span className="navicon"></span>
+              </label>
+              <ul className="menu">
+                <li>
+                  <a href="#work">Our Work</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#careers">Careers</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="social-links">
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/">
+                <span>
+                  <IconLinkedInSVG />
+                </span>
+              </a>
+              <a target="_blank" rel="noreferrer" href="https://github.com/momcilo-mitrovic">
+                <span>
+                  <IconGithubSVG />
+                </span>
+              </a>
+            </div>
           </div>
         </header>
       </div>
