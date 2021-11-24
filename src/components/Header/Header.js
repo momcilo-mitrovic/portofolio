67dca201c2c6f496f87754da9f1af0ca9e55eff2
Schalk Burger <schalkb@gmail.com>
2021-11-24 19:38:41 +0200

    Mobile menu shit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,46 +32,48 @@ function Header() {
             </ul>
           </nav>
 
-          <nav className="menu-mobile">
-            <input className="menu-btn" type="checkbox" id="menu-btn" />
-            <label className="menu-icon" for="menu-btn">
-              <span className="navicon"></span>
-            </label>
-            <ul className="menu">
-              <li>
-                <a href="#work">Our Work</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#careers">Careers</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
+          <div className="menu-mobile-container">
+            <nav className="menu-mobile">
+              <input className="menu-btn" type="checkbox" id="menu-btn" />
+              <label className="menu-icon" for="menu-btn">
+                <span className="navicon"></span>
+              </label>
+              <ul className="menu">
+                <li>
+                  <a href="#work">Our Work</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#careers">Careers</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </nav>
 
-          <div className="social-links">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/"
-            >
-              <span>
-                <IconLinkedInSVG />
-              </span>
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/momcilo-mitrovic"
-            >
-              <span>
-                <IconGithubSVG />
-              </span>
-            </a>
+            <div className="social-links">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/momcilo-mitrovic-548849224/"
+              >
+                <span>
+                  <IconLinkedInSVG />
+                </span>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/momcilo-mitrovic"
+              >
+                <span>
+                  <IconGithubSVG />
+                </span>
+              </a>
+            </div>
           </div>
         </header>
       </div>
