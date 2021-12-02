361b6fb43dd21becd0ae912535c8cec7f1c00806
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-12-03 00:11:09 +0100

    Fixed the services and made it responsive
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,8 +1,8 @@
-import "./Services.scss";
-import Card from "../../components/Card/Card";
-import IconDev from "../../components/Card/IconDev";
-import IconDesign from "../../components/Card/IconDesign";
-import IconApps from "../../components/Card/IconApps";
+import './Services.scss'
+import Card from '../../components/Card/Card'
+import IconDev from '../../components/Card/IconDev'
+import IconDesign from '../../components/Card/IconDesign'
+import IconApps from '../../components/Card/IconApps'
 
 function Services() {
   return (
@@ -12,35 +12,25 @@ function Services() {
           <h1> Services </h1>
         </div>
       </div>
-      <div className="container flex">
+      <div className="Services-cards">
         <Card>
           <IconDesign />
           <h3>Web Design</h3>
-          <p>
-            Colors, fonts, typography, components... all in one place,
-            documented and easy to access.
-          </p>
+          <p>Colors, fonts, typography, components... all in one place, documented and easy to access.</p>
         </Card>
         <Card>
           <IconDev />
           <h3> Full Stack Development </h3>
-          <p>
-            Cutting-edge and mobile responsive HTML5, CSS and React
-            applications. I've got you covered.
-          </p>
+          <p>Cutting-edge and mobile responsive HTML5, CSS and React applications. I've got you covered.</p>
         </Card>
         <Card>
           <IconApps />
           <h3> Single-page Apps </h3>
-          <p>
-            {" "}
-            Modern React apps that are super fast and setup with no
-            configuration.
-          </p>
+          <p> Modern React apps that are super fast and setup with no configuration.</p>
         </Card>
       </div>
     </div>
-  );
+  )
 }
 
-export default Services;
+export default Services
