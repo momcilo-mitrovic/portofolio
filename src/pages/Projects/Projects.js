daf74facbaba3ae4d12c43c31ff54b5edb14cb34
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-12-19 20:39:24 +0100

    added projects page and fetched the api from facebook github thing
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,37 @@
 import './Projects.scss'
+import ReactDOM from 'react-dom'
 import { Helmet } from 'react-helmet'
+
+function requestUserRepos(userName) {
+  // Create new XMLHttpRequest object
+  const xhr = new XMLHttpRequest()
+
+  // GitHub endpoint, dynamically passing in specified username
+  const url = `https://api.github.com/users/${userName}/repos`
+
+  // Open a new connection, using a GET request via URL endpoint
+  // Providing 3 arguments (GET/POST, The URL, Async True/False)
+  xhr.open('GET', url, true)
+
+  // When request is received
+  // Process it here
+  xhr.onload = function () {
+    // Parse API data into JSON
+    const data = JSON.parse(this.response)
+
+    // Log the response
+    console.log(data)
+  }
+
+  // Send the request to the server
+  xhr.send()
+}
 
 function Projects() {
   return (
     <div className="Projects">
+      {requestUserRepos('facebook')}
+
       <Helmet>
         <meta charSet="utf-8" />
         <title>Momcilo Mitrovic Portofolio Projects</title>
