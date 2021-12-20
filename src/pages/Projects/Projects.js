24786ea6db3ddcb0c32aaa4fdd6130fea8c82195
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-12-20 22:44:51 +0100

    Projects page works, responsive and its calling the api correctly
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,4 @@
 import './Projects.scss'
-import ReactDOM from 'react-dom'
-import { Helmet } from 'react-helmet'
 
 function requestUserRepos(username) {
   // Create new XMLHttpRequest object
@@ -65,24 +63,24 @@ function requestUserRepos(username) {
   // Send the request to the server
   xhr.send()
 }
+
 function Projects() {
   return (
-    requestUserRepos('facebook')
     <div className="Projects">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Momcilo Mitrovic Portofolio Projects</title>
-      </Helmet>
       <div className="Projects-header-container">
         <div className="container Projects-header">
-          <h1> Projects </h1>
+          <h1>Projects</h1>
           <p>Here are some projects i have made</p>
         </div>
       </div>
+      <div className="container flex flex-column">
+        <h2>Repos list</h2>
+
+        {requestUserRepos('momcilo-mitrovic')}
+        <ul id="userRepos">{/* Repos list */}</ul>
+      </div>
     </div>
   )
-  {
-  }
 }
 
 export default Projects
