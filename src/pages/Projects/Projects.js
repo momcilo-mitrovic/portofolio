ba541405378c3b8269620cea92b804d15671fd6d
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2021-12-20 20:00:42 +0100

    I fucked up the project one
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -2,12 +2,12 @@ import './Projects.scss'
 import ReactDOM from 'react-dom'
 import { Helmet } from 'react-helmet'
 
-function requestUserRepos(userName) {
+function requestUserRepos(username) {
   // Create new XMLHttpRequest object
   const xhr = new XMLHttpRequest()
 
   // GitHub endpoint, dynamically passing in specified username
-  const url = `https://api.github.com/users/${userName}/repos`
+  const url = `https://api.github.com/users/${username}/repos`
 
   // Open a new connection, using a GET request via URL endpoint
   // Providing 3 arguments (GET/POST, The URL, Async True/False)
@@ -18,35 +18,57 @@ function requestUserRepos(userName) {
   xhr.onload = function () {
     // Parse API data into JSON
     const data = JSON.parse(this.response)
+    let root = document.getElementById('userRepos')
+    while (root.firstChild) {
+      root.removeChild(root.firstChild)
+    }
+    if (data.message === 'Not Found') {
+      let ul = document.getElementById('userRepos')
 
-    // Log the response
-    console.log(data)
+      // Create variable that will create li's to be added to ul
+      let li = document.createElement('li')
 
-    // Loop over each object in data array
-    for (let i in data) {
-      // Log the repo name
-      console.log('Repo:', data[i].name)
+      // Add Bootstrap list item class to each li
+      li.classList.add('list-group-item')
+      // Create the html markup for each li
+      li.innerHTML = `
+              <p><strong>No account exists with username:</strong> ${username}</p>`
+      // Append each li to the ul
+      ul.appendChild(li)
+    } else {
+      // Get the ul with id of of userRepos
+      let ul = document.getElementById('userRepos')
+      let p = document.createElement('p')
+      p.innerHTML = `<p><strong>Number of Public Repos:${data.length}</p>`
+      ul.appendChild(p)
+      // Loop over each object in data array
+      for (let i in data) {
+        // Create variable that will create li's to be added to ul
+        let li = document.createElement('li')
 
-      // Log the repo description
-      console.log('Description:', data[i].description)
+        // Add Bootstrap list item class to each li
+        li.classList.add('list-group-item')
 
-      // Log the repo url
-      console.log('URL:', data[i].html_url)
+        // Create the html markup for each li
+        li.innerHTML = `
+              <p><strong>Repo:</strong> ${data[i].name}</p>
+              <p><strong>Description:</strong> ${data[i].description}</p>
+              <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+          `
 
-      // Add a separator between each repo
-      console.log('=========================')
+        // Append each li to the ul
+        ul.appendChild(li)
+      }
     }
   }
 
   // Send the request to the server
   xhr.send()
 }
-
 function Projects() {
   return (
+    requestUserRepos('facebook')
     <div className="Projects">
-      {requestUserRepos('momcilo-mitrovic')}
-
       <Helmet>
         <meta charSet="utf-8" />
         <title>Momcilo Mitrovic Portofolio Projects</title>
@@ -59,6 +81,8 @@ function Projects() {
       </div>
     </div>
   )
+  {
+  }
 }
 
 export default Projects
