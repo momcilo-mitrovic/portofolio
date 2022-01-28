7537d8f8e12201390a8aca06ed8dc397707251c9
Schalk Burger <schalkb@gmail.com>
2022-01-28 20:01:24 +0200

    Toggle fix sortof

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,19 +1,26 @@
-import './Toggle.scss'
+import "./Toggle.scss";
 
-import IconSunSVG from './IconSun'
-import IconMoonSVG from './IconMoon'
+import IconSunSVG from "./IconSun";
+import IconMoonSVG from "./IconMoon";
 
 const Toggle = ({ checked, onChange }) => (
   <div className="toggle-container">
     <IconSunSVG />
     <span className="toggle-control">
       <form>
-        <input className="dmcheck" type="checkbox" checked={checked} onChange={onChange} id="dmcheck" />
-        <label htmlFor="dmcheck" />
+        <label className="dmcheck-label" htmlFor="dmcheck" />
+        <input
+          className="dmcheck"
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          id="dmcheck"
+          aria-labelledBy="dmcheck"
+        />
       </form>
     </span>
     <IconMoonSVG />
   </div>
-)
+);
 
-export default Toggle
+export default Toggle;
